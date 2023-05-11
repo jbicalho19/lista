caacc661f1858ac31048ab5f60d07c322c226a81
jbicalho19 <j_bicalho19@yahoo.com.br>
2023-05-11 17:50:02 -0300

    Primeira entrada de dados (seed) de games

--- src/main/java/com/bicalho/lista/entities/Jogo.java
+++ src/main/java/com/bicalho/lista/entities/Jogo.java
@@ -23,7 +23,11 @@ public class Jogo {
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Jogo() {
